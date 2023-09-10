4f1fa0d60a8de7db557d5b3a114bb31a2adcdafe
gayashan4lk <gayashan4lk@gmail.com>
2023-09-10 06:17:55 +0530

    add car models

---
+++ Models/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Models
+{
+	public class Car : ICar
+	{
+		public string Color { get; set; }
+		private int _speed;
+		public int Speed { get; set; }
+
+		public void Accelerate(int acceleration)
+		{
+			_speed += acceleration;
+		}
+
+		public bool IsMoving()
+		{
+			return (_speed != 0);
+		}
+
+		public Car()
+		{
+			Color = "White";
+			_speed = 0;
+		}
+
+		public Car(string color, int speed)
+		{
+			Color = color;
+			Speed = speed;
+		}
+
+		public double CalculateMpg(int startKm, int endKm, double fuelConsumptionInLiters)
+		{
+			return (endKm -  startKm) / fuelConsumptionInLiters;
+		}
+	}
+}
---
+++ Models/SportsCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Models
+{
+	public class SportsCar : Car
+	{
+		public SportsCar()
+		{
+			Color = "Green";
+		}
+	}
+}
