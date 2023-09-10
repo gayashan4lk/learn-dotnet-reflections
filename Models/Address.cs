abae917a5e12d5280e2c4531e066ee1640ff69ae
gayashan4lk <gayashan4lk@gmail.com>
2023-09-10 05:38:02 +0530

    add models

---
+++ Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Models
+{
+	public class Address
+	{
+		public string Line1 { get; set; }
+		public string Line2 { get; set; }
+		public string City { get; set; }
+	}
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Models
+{
+	public class Customer
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public Address Address { get; set; }
+
+		private string Wealth;
+
+		public void SayHello()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void CalculateNetWealth()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
